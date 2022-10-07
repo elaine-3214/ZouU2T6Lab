6df37a8ce07f19a48f8d8f82c59d22d90140ca3f
1E10 Laptop <sample@bths.edu>
2022-10-07 12:29:05 -0400

    lab

---
+++ src/LabRunner.java
@@ -0,0 +1,12 @@
+public class LabRunner
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Hello, friend!\n";
+        System.out.print(greeting);
+        System.out.print("Nice to see you!\n");
+        System.out.println("Goodbye!");
+
+
+    }
+}
