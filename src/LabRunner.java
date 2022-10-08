bbb82380e72823b985949935ff51eaa5a2606059
elaine <elainez13@nycstudents.net>
2022-10-08 15:47:47 -0400

    done

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -2,11 +2,17 @@ public class LabRunner
 {
     public static void main(String[] args)
     {
-        String greeting = "Hello, friend!\n";
-        System.out.print(greeting);
-        System.out.print("Nice to see you!\n");
-        System.out.println("Goodbye!");
+//        Student student = new Student("Abby", "Smith", 2023);
+//        student.addTestScore(95);
+//        student.addTestScore(98);
+//        System.out.println(student.getStudentInfo());
+        int one = 1;
 
+        int two = 2;
+
+        String zee = "Z";
+
+        System.out.println(one + two + zee);
 
     }
 }
